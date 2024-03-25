29854df75f4effd99307ae6964c2701a13b00079
Team6911 <155586579+jkim35@users.noreply.github.com>
2024-03-25 14:33:56 -0400

    i ahte this

--- digit_sum.java
+++ digit_sum.java
@@ -4,14 +4,21 @@ public class digit_sum {
         int orig;
         int newint;
         int remain;
+        int remenants;
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a positive number: ");
         orig = input.nextInt();
         remain = orig;
         newint = orig;
-        while(!(newint<=9)){
+        if(remain <= 9){
+            System.out.println(remain);
+        }
+        while(!(remain<=9)){
             newint += remain%10;
+            remenants = remain%10;
+             remain -= remenants;
             System.out.println(newint);
+            System.out.println(remain);
         }
     }
 }
